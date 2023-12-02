a934f0d74f97b137e25626b90650f193c64a24b0
Jerry <superjerry88@hotmail.com>
2023-12-02 13:21:33 +0800

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
