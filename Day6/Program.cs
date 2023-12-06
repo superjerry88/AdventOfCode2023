e1f7922dff8264ec39612052ed97c42edffc4f7f
Jerry <superjerry88@hotmail.com>
2023-12-06 14:32:57 +0800

    Day6 - Added quadratic equation to solve it at constant speed

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,4 +1,7 @@
 ﻿//var lines = File.ReadAllLines("Example.txt");
+
+using System.Diagnostics;
+
 var lines = File.ReadAllLines("Input.txt");
 
 var times = lines[0].Split(' ').Skip(1).Where(c => !string.IsNullOrEmpty(c)).Select(long.Parse).ToList();
@@ -7,13 +10,13 @@ var distances = lines[1].Split(' ').Skip(1).Where(c => !string.IsNullOrEmpty(c))
 long combination = 1;
 for (var i = 0; i < times.Count; i++)
 {
-    combination *= PossibleWin(times[i], distances[i]);
+    combination *= PossibleWin2(times[i], distances[i]);
 }
 Console.WriteLine($"[1] Total combination: {combination}");
 
 var singleTime = long.Parse(string.Join("", times));
 var singleDistance = long.Parse(string.Join("", distances));
-Console.WriteLine($"[2] Total combination: {PossibleWin(singleTime, singleDistance)}");
+Console.WriteLine($"[2] Total combination: {PossibleWin2(singleTime, singleDistance)}");
 return;
 
 
@@ -26,7 +29,19 @@ long PossibleWin(long timeLimit, long distanceLimit)
         {
             ways++;
         }
+        
     }
+    return ways;
+}
+
+long PossibleWin2(long timeLimit, long distanceLimit)
+{
+    var discriminant = Math.Sqrt(timeLimit * timeLimit - 4 * distanceLimit);
+
+    var root1 = (timeLimit - discriminant) / 2;
+    var root2 = (timeLimit + discriminant) / 2;
+
+    var ways = (long)Math.Floor(root2) - (long)Math.Ceiling(root1) + 1;
     return ways;
 }
 
